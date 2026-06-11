3d5fa346bd7cb7a6d7d227ee41992f5daf8a4096
Piotr Stachowicz <337942@uwr.edu.pl>
2026-06-11 23:06:59 +0200

    Fixed constructors for optuna

--- src/models/classic.py
+++ src/models/classic.py
@@ -12,32 +12,69 @@ def build_dummy_classifier():
     )
 
 
-def build_logistic_regression():
+def build_logistic_regression(
+    penalty="l2",
+    C=1.0,
+    solver="lbfgs",
+    max_iter=1000,
+    l1_ratio=0.0
+):
     return LogisticRegression(
-        class_weight="balanced",
-        max_iter=1000,
+        penalty=penalty,
+        C=C,
         random_state=SEED,
+        solver=solver,
+        max_iter=max_iter,
+        l1_ratio=l1_ratio
     )
 
 
-def build_random_forest():
+def build_random_forest(
+    n_estimators=300,
+    criterion="gini",
+    max_depth=None,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    max_features="sqrt",
+):
     return RandomForestClassifier(
-        n_estimators=300,
-        class_weight="balanced",
-        random_state=SEED,
+        n_estimators=n_estimators,
+        criterion=criterion,
+        max_depth=max_depth,
+        min_samples_split=min_samples_split,
+        min_samples_leaf=min_samples_leaf,
+        max_features=max_features,
         n_jobs=-1,
+        random_state=SEED
     )
 
 
-def build_xgboost(scale_pos_weight: float):
+def build_xgboost(
+    n_estimators=500,
+    max_depth=6,
+    learning_rate=0.05,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    gamma=0,
+    min_child_weight=1,
+    scale_pos_weight=None,
+    reg_alpha=0,
+    reg_lambda=0,
+):
     return XGBClassifier(
-        n_estimators=500,
-        max_depth=6,
-        learning_rate=0.05,
-        subsample=0.8,
-        colsample_bytree=0.8,
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        learning_rate=learning_rate,
+        subsample=subsample,
+        colsample_bytree=colsample_bytree,
+        gamma=gamma,
+        min_child_weight=min_child_weight,
         scale_pos_weight=scale_pos_weight,
+        objective="binary:logistic",
         eval_metric="logloss",
         random_state=SEED,
         n_jobs=-1,
+        tree_method="hist",
+        reg_alpha=reg_alpha,
+        reg_lambda=reg_lambda
     )
